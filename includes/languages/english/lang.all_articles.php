3f1a35479d8c1db471fa18d23ac16e279b91f2c5
lat9 <lat9@vinosdefrutastropicales.com>
2024-01-31 19:26:45 -0500

    Missed a couple of language files

---
+++ includes/languages/english/lang.all_articles.php
@@ -0,0 +1,14 @@
+<?php
+// -----
+// Part of the News Box Manager plugin, re-structured for Zen Cart v1.5.8a and later by lat9.
+// Copyright (C) 2018-2024, Vinos de Frutas Tropicales
+//
+$define = [
+    'NAVBAR_TITLE' => 'All %s Articles',          //- The %s is filled in with the sub-content type.
+    'HEADING_TITLE' => 'All %s Articles',         //- The %s is filled in with the sub-content type.
+
+    'TEXT_NO_NEWS_CURRENTLY' => 'We currently have no articles published.  Check back soon!',
+
+    'TEXT_DISPLAY_NUMBER_OF_NEWS_ARTICLES' => 'Displaying <strong>%u</strong> to <strong>%u</strong> (of <strong>%u</strong> articles)',
+];
+return $define;
---
+++ includes/languages/english/lang.article.php
@@ -0,0 +1,18 @@
+<?php
+// -----
+// Part of the News Box Manager plugin, re-structured for Zen Cart v1.5.8a and later by lat9.
+// Copyright (C) 2015-2024, Vinos de Frutas Tropicales
+//
+// +----------------------------------------------------------------------+
+// | Do Not Remove: Coded for Zen-Cart by geeks4u.com                     |
+// | Dedicated to Memory of Amelita "Emmy" Abordo Gelarderes              |
+// +----------------------------------------------------------------------+
+//
+$define = [
+    'NAVBAR_TITLE' => 'News',
+    'HEADING_TITLE' => 'News: ',
+    'TEXT_NEWS_PUBLISHED_DATE' => 'Article Date(s):',
+
+    'TEXT_NEWS_ARTICLE_NOT_FOUND' => 'Sorry, that article is no longer available for viewing.  Displaying <em>all</em> articles instead.',
+];
+return $define;
